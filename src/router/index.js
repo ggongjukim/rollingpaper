6ceaa4fb660066a079b78873cdf1590c6e71e749
ggongjukim <kch7892003@gmail.com>
2024-05-01 18:57:01 +0900

    ✨ 초기세팅

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from 'react-router-dom';
+
+import { MainPage, ReservationsPage } from '../pages';
+
+export default function Router() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navigate to='/main' replace />,
+    },
+    {
+      path: '/main',
+      element: <MainPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
